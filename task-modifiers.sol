2c6f7d5679e74290ef76ba04cbefc064e52185a8
sebyandu <podaru_sebastian@yahoo.com>
2023-01-03 19:07:15 +0200

    Create task-modifiers.sol
    This task extends the functionality of the previous task. Create a deposit function that allows anybody to send funds. Store the user and the amount in a mapping as the previous task.
    
    Add a withdraw function and create a modifier that only allows the owner of the contract to withdraw the funds.
    Add an addFund function and create a modifier that only allows users that have deposited using the deposit function, to increase their balance on the mapping. The function should accept the amount to be added and update the mapping to have the new balance
    Hint: if their balance is zero on the mapping, it should revert
    
    Hint: theMapping[userId] = theMapping[userId] + _amount;
    
    Create a modifier that accepts a value(uint256 _amount):
    Create a private constant variable called Fee
    In the modifier check if the value(_amount) it accepts is less than the Fee, revert with a custom error AmountToSmall()
    Add it to the addFund function
    Hint: addFund(uint256 _amount)..........

---
+++ task-modifiers.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.0 <0.9.0;
+struct User {
+    string name;
+    uint256 age;
+}
+error AmountTooSmall();
+
+/**
+ * @title MySavings
+ * @dev Deposit savings
+ */
+contract MySavings {
+    address public owner;
+
+    mapping(address => uint256) public balance;
+    mapping(address => User) public userInfo;
+    uint256 private constant _fee = 10; // missing fee value specification
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Modifier to check that the caller is the owner of the contract.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    /**
+     * @dev Check that the caller used previously the deposit functionality(with strictly positiv amounts).
+     */
+    modifier hasDeposit() {
+        require(checkBalance() > 0, "Balance is 0");
+        _;
+    }
+    /**
+     * @dev Check that the amount covers the fees at least.
+     * @param _amount ->value to compare with the fee
+     */
+    modifier greaterThanFee(uint256 _amount) {
+        // more efficient alternative for
+        // require(_amount > _fee, "AmountTooSmall");
+        if (_amount < _fee) revert AmountTooSmall();
+        _;
+    }
+
+    /**
+     * @dev Deposit the value,
+     * @param amount -> amount to store for the current sender
+     */
+    function deposit(uint256 amount) public payable {
+        balance[msg.sender] += amount;
+    }
+
+    /**
+     *@dev Get the sender balance
+     */
+    function checkBalance() public view returns (uint256) {
+        return balance[msg.sender];
+    }
+
+    /**
+     * @dev set KYC for User(caller)
+     * @param name -> name of the curent user
+     * @param age -> age of the curent user
+     */
+    function setUserDetails(string calldata name, uint256 age) external {
+        userInfo[msg.sender] = User(name, age);
+    }
+
+    /**
+     *@dev get KYC infos for User(caller)
+     */
+    function getUserDetail() external view returns (User memory) {
+        return userInfo[msg.sender];
+    }
+
+    /**
+     *@dev Withdraw the funds(owner)
+     */
+    function withdraw() external onlyOwner {}
+
+    //     Add an addFund function and create a modifier that only allows users that have deposited using the deposit function, to increase their balance on the mapping.
+    //     The function should accept the amount to be added and update the mapping to have the new balance
+    //     Hint: if their balance is zero on the mapping, it should revert
+
+    // Hint: theMapping[userId] = theMapping[userId] + _amount;
+
+    function addFund(uint256 _amount)
+        external
+        hasDeposit
+        greaterThanFee(_amount)
+    {
+        deposit(_amount);
+    }
+}
